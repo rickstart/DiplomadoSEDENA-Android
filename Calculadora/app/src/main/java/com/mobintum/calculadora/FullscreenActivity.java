23e4b0f08674009f457402cfd27cd011a43d7363
Ricardo Centeno Lugo <Rick@MacBook-Pro-de-Ricardo-2.local>
2015-12-12 13:20:43 -0600

    Calculadora make basic operations

--- Calculadora/app/src/main/java/com/mobintum/calculadora/FullscreenActivity.java
+++ Calculadora/app/src/main/java/com/mobintum/calculadora/FullscreenActivity.java
@@ -1,23 +1,24 @@
 package com.mobintum.calculadora;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
 
 public class FullscreenActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnZero,btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine;
     private Button btnAC, btnPlusLess, btnPercent, btnDivision, btnMultiply, btnLess, btnPlus, btnEqual, btnDot ;
     private TextView textResult;
-    private int oper1, oper2, result;
+    private Double oper1=0.0, oper2, result;
+    private Boolean flagOper = false;
+    private final static int SUM=1;
+    private final static int MULTIPLY=2;
+    private final static int DIVISION=3;
+    private final static int SUBSTRACTION=4;
+    private final static int PERCENT=5;
+    private int typeOper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,28 +68,95 @@ public class FullscreenActivity extends AppCompatActivity implements View.OnClic
 
     }
 
+    public String formatResult(Double result){
+        String strResult = Double.toString(result);
+        String number[] = strResult.split("\\.");
+        if(Integer.parseInt(number[1])==0){
+            return number[0];
+        }else{
+            return strResult;
+        }
+    }
 
+    public Double operation(int typeOper, double oper1, double oper2){
+        Double result = 0.0;
+        switch (typeOper){
+            case SUM:
+                result = oper1 + oper2;
+                break;
+            case SUBSTRACTION:
+                result = oper1 - oper2;
+                break;
+            case MULTIPLY:
+                result = oper1 * oper2;
+                break;
+            case DIVISION:
+                result = oper1 / oper2;
+                break;
+        }
+        return result;
+    }
+
+    public void setOper(){
+        Double number = Double.parseDouble(textResult.getText().toString());
+        if(number!=0.0 && oper1==0.0){
+            oper1 = number;
+        }else if(oper1!=0.0 && flagOper){
+            oper1 = operation(typeOper, oper1, number);
+            textResult.setText(formatResult(oper1));
+        }
+        flagOper = false;
+    }
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btnPlus:
+        Double number = 0.0;
 
+        switch (v.getId()) {
+
+            case R.id.btnLess:
+                setOper();
+                typeOper = SUBSTRACTION;
+                break;
+
+            case R.id.btnPlus:
+                setOper();
+                typeOper = SUM;
+                break;
+            case R.id.btnDivision:
+                setOper();
+                typeOper = DIVISION;
+                break;
+            case R.id.btnMultiply:
+                setOper();
+                typeOper = MULTIPLY;
+                break;
+            case R.id.btnAC:
+                textResult.setText("0");
+                oper1 = 0.0;
+                flagOper = false;
+                break;
+            case R.id.btnEqual:
+                if(oper1!=0.0 && flagOper){
+                    number = Double.parseDouble(textResult.getText().toString());
+                    oper1 = operation(typeOper, oper1, number);
+                    textResult.setText(formatResult(oper1));
+                    flagOper = false;
+                }
 
                 break;
             default:
                 Button btnTemp = (Button) v;
-                if(oper1==0)
-                    oper1 = Integer.parseInt(btnTemp.getText().toString());
-                else {
-                    result = oper1 + Integer.parseInt(btnTemp.getText().toString());
-                    textResult.setText(Integer.toString(result));
-                    oper1=0;
+                String text = btnTemp.getText().toString();
+                number = Double.parseDouble(textResult.getText().toString());
+                if(number!=0.0 && flagOper) {
+                    textResult.append(text);
+                }else{
+                    textResult.setText(text);
+                    flagOper = true;
                 }
-
                 break;
 
         }
-
 
     }
 }
