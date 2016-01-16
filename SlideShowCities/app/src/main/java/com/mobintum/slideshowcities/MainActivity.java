60ea65d9569aa7f274312761a7755bb76a3b3ed6
Ricardo Centeno Lugo <Rick@MacBook-Pro-de-Ricardo-2.local>
2016-01-16 11:29:25 -0600

    SlideShow Cities advance

--- SlideShowCities/app/src/main/java/com/mobintum/slideshowcities/MainActivity.java
+++ SlideShowCities/app/src/main/java/com/mobintum/slideshowcities/MainActivity.java
@@ -3,6 +3,7 @@ package com.mobintum.slideshowcities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton btnBack, btnNext;
+    Button btnGo;
     ImageView photo;
     ArrayList<City> cities = new ArrayList<>();
     int index = 0;
@@ -24,14 +26,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnNext = (ImageButton) findViewById(R.id.btnNext);
         btnNext.setOnClickListener(this);
         photo = (ImageView) findViewById(R.id.photo);
-
-        City huatulco = new City("Huatulco","1000000","Oaxaca Beach", getResources().getDrawable(R.drawable.ic_back));
+        btnGo = (Button) findViewById(R.id.btnGo);
+        City huatulco = new City("Huatulco","1000000","Oaxaca Beach", getResources().getDrawable(R.drawable.huatulco));
+        City acapulco = new City("Acapulco","1000000","Acapulco Beach", getResources().getDrawable(R.drawable.acapulco));
+        City holbox = new City("Holbox","1000000","Holbox Beach", getResources().getDrawable(R.drawable.holbox));
+        City cabos = new City("Los Cabos","1000000","Los Cabos Beach", getResources().getDrawable(R.drawable.los_cabos));
+        City tulum = new City("Tulum","1000000","Tulum Beach", getResources().getDrawable(R.drawable.tulum));
         cities.add(huatulco);
-
+        cities.add(acapulco);
+        cities.add(holbox);
+        cities.add(cabos);
+        cities.add(tulum);
         loadData();
-
-
-
     }
 
     private void loadData(){
@@ -54,6 +60,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     index++;
                     loadData();
                 }
+                break;
+            case R.id.btnGo:
                 break;
         }
 
