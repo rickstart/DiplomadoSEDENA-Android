5163eab8cdc3a946569649fa4c687640c62b4f5a
Ricardo Centeno Lugo <Rick@MacBook-Pro-de-Ricardo-2.local>
2016-01-23 14:09:17 -0600

    Agenda advance

--- Agenda/app/src/main/java/com/mobintum/agenda/ArrayAdapterContact.java
+++ Agenda/app/src/main/java/com/mobintum/agenda/ArrayAdapterContact.java
@@ -8,6 +8,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+
 /**
  * Created by Rick on 23/01/16.
  */
@@ -16,17 +19,34 @@ public class ArrayAdapterContact extends ArrayAdapter<Contact> {
     private int resource;
     private Contact[] objects;
     private LayoutInflater inflater;
+    private Context context;
 
     public ArrayAdapterContact(Context context, int resource, Contact[] objects) {
         super(context, resource, objects);
         this.resource = resource;
         this.objects = objects;
         this.inflater = LayoutInflater.from(context);
+        this.context = context;
 
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        Viewholder holder;
+        if(convertView == null){
+            convertView = inflater.inflate(resource,parent, false);
+            holder = new Viewholder();
+            holder.photo = (ImageView) convertView.findViewById(R.id.photo);
+            holder.txtName = (TextView) convertView.findViewById(R.id.txtName);
+            convertView.setTag(holder);
+        }else{
+            holder = (Viewholder) convertView.getTag();
+        }
+
+        Picasso.with(context).load(objects[position].getPhoto()).into(holder.photo);
+        holder.txtName.setText(objects[position].getName());
+
+
         return super.getView(position, convertView, parent);
     }
 
