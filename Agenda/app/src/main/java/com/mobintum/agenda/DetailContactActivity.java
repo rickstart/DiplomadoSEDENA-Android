eace6ef9961847f8193737bd5979e3004623cd99
Ricardo Centeno Lugo <Rick@MacBook-Pro-de-Ricardo-2.local>
2016-01-30 10:41:48 -0600

    agenda connect activity with layout

--- Agenda/app/src/main/java/com/mobintum/agenda/DetailContactActivity.java
+++ Agenda/app/src/main/java/com/mobintum/agenda/DetailContactActivity.java
@@ -1,11 +1,18 @@
 package com.mobintum.agenda;
 
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailContactActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView photo;
@@ -35,12 +42,35 @@ public class DetailContactActivity extends AppCompatActivity implements View.OnC
 
     @Override
     public void onClick(View v) {
+        Intent intent;
         switch (v.getId()){
             case R.id.btnEmail:
+                intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:ricrdo.celj@gmail.com"));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Esta es una prueba de tema");
+                intent.putExtra(Intent.EXTRA_TEXT, "Este mensaje fue generado desde Java");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"ricardo.celj@gmail.com","mobilestudiotest@gmail.com"});
+
+                try {
+                    startActivity(intent);
+                }catch (ActivityNotFoundException e){
+                    e.printStackTrace();
+                    Toast.makeText(this, "No tienes un email configurado", Toast.LENGTH_SHORT).show();
+                }
+
                 break;
             case R.id.btnPhone:
+                intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:(521) 5514382887"));
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "No tienes el permiso para llamar", Toast.LENGTH_SHORT).show();
+                }else{
+                    startActivity(intent);
+                }
+
                 break;
             case R.id.btnGithub:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.github.com"));
+
+                startActivity(intent);
                 break;
         }
     }
