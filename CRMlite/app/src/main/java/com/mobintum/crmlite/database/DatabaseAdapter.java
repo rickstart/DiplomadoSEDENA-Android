71a3accee00d39783ccd547f58b79f8179abdff3
Ricardo Centeno Lugo <Rick@MacBook-Pro-de-Ricardo-2.local>
2016-05-07 11:15:26 -0500

    Create database to CRM

---
+++ CRMlite/app/src/main/java/com/mobintum/crmlite/database/DatabaseAdapter.java
@@ -0,0 +1,45 @@
+package com.mobintum.crmlite.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ *
+ * Created by Rick on 20/08/15.
+ * All right are reserved for Rick
+ *
+ */
+public class DatabaseAdapter {
+
+    private static DatabaseHelper mDbHelper;
+    private static SQLiteDatabase mDb;
+    private static Context context;
+
+    public DatabaseAdapter(Context context) {
+        this.context = context;
+    }
+
+    public static boolean openDB(Context context){
+        if(mDbHelper != null)
+            mDbHelper.close();
+        mDbHelper = new DatabaseHelper(context);
+
+        try {
+            mDb = mDbHelper.getWritableDatabase();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static SQLiteDatabase getDB(){
+
+        if(mDb == null)
+            openDB(context);
+
+        return mDb;
+
+    }
+
+}
