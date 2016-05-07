d64d978c06c07144acfd87da76d51aa376d03cce
Ricardo Centeno Lugo <Rick@MacBook-Pro-de-Ricardo-2.local>
2016-05-07 12:09:30 -0500

    Dialog created

--- CRMlite/app/src/main/java/com/mobintum/crmlite/activities/MainActivity.java
+++ CRMlite/app/src/main/java/com/mobintum/crmlite/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.mobintum.crmlite.activities;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,22 +11,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.mobintum.crmlite.R;
+import com.mobintum.crmlite.dialogs.AddCustomerDialog;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        fm = getSupportFragmentManager();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                AddCustomerDialog dialog = new AddCustomerDialog();
+                dialog.show(fm,"Dialog");
+
             }
         });
     }
--- CRMlite/app/src/main/java/com/mobintum/crmlite/dialogs/AddCustomerDialog.java
+++ CRMlite/app/src/main/java/com/mobintum/crmlite/dialogs/AddCustomerDialog.java
@@ -1,10 +1,63 @@
 package com.mobintum.crmlite.dialogs;
 
-import android.app.DialogFragment;
+import android.app.Dialog;
+import android.support.v4.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+
+import com.iangclifton.android.floatlabel.FloatLabel;
+import com.mobintum.crmlite.R;
 
 /**
  * Created by Rick on 07/05/16.
  * email: ricardo.centeno@mobintum.com
  */
-public class AddCustomerDialog extends DialogFragment{
+public class AddCustomerDialog extends DialogFragment implements View.OnClickListener{
+
+    private FloatLabel etName, etBusinessName, etRFC,etNotes;
+    private Button btnCancel, btnSave;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.dialog_add_customer,container,false);
+        etName = (FloatLabel) view.findViewById(R.id.etName);
+        etBusinessName = (FloatLabel) view.findViewById(R.id.etBusinessName);
+        etRFC = (FloatLabel) view.findViewById(R.id.etRFC);
+        etNotes = (FloatLabel) view.findViewById(R.id.etNotes);
+        btnCancel = (Button) view.findViewById(R.id.btnCancel);
+        btnSave = (Button) view.findViewById(R.id.btnSave);
+        return view;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+        // request a window without the title
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnCancel:
+                break;
+            case R.id.btnSave:
+                save();
+                break;
+        }
+
+    }
+
+    private boolean save(){
+        return false;
+    }
 }
